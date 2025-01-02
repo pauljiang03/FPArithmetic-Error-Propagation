a669a7c702a13e0e808441c6117a10cbfc6250d9
pauljiang03 <jiang861@purdue.edu>
2025-01-02 17:20:49 -0500

    fpmul in progress

--- try2.py
+++ try2.py
@@ -10,13 +10,10 @@ def fp16_to_fp32(x):
     return fpFPToFP(RNE(), x, Float32)
 def fp64_to_fp32(x):
     return fpFPToFP(RNE(), x, Float32)
-
 def fp32_to_fp16(x):
     return fpFPToFP(RNE(), x, Float16)
-
 def fp_to_bv(fp, sort):
     return fpToIEEEBV(fp)
-
 def bv_to_binary_str(bv):
     return bin(bv.as_long())[2:].zfill(bv.size())
 
@@ -46,8 +43,8 @@ t_32 = fp16_to_fp32(t)
 v_32_fin = v_32 * t_32
 z_32_fin = a_32 * t_32 * t_32
 
-sum_16 = fpAdd(RNE(), x_16, v_16)
-sum_16 = fpAdd(RNE(), z_16, sum_16)
+sum_16 = fpAdd(RTZ(), x_16, v_16)
+sum_16 = fpAdd(RTZ(), z_16, sum_16)
 sum_32 = fpAdd(RNE(), x_32, v_32_fin)
 sum_32 = fpAdd(RNE(), z_32_fin, sum_32)
 
@@ -59,7 +56,7 @@ s.add(v_0 == 100)
 s.add(Not(fpIsInf(compare_16)))
 s.add(sum_32 != compare_16)
 diff = If(compare_16 > sum_32, fpSub(RNE(), compare_16, sum_32), fpSub(RNE(), sum_32, compare_16))
-s.add(diff > sum_32 / 920.5)
+s.add(diff > sum_32 / 558.5)
 
 if s.check() == sat:
     m = s.model()
